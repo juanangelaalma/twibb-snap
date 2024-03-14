a923ce1f363e89778c821372d517ba86a79c17f6
juanangelaalma <juanangelaalma@gmail.com>
2024-03-14 07:16:55 +0700

    separated per component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
+import Canvas from '../components/Canvas'
+import CameraButton from "@/components/CameraButton";
+import DownloadButton from "@/components/DownloadButton"
 
 export default function Home() {
   const [overlayFilled, setOverlayFilled] = useState<Boolean>(false)
@@ -47,63 +50,15 @@ export default function Home() {
     <main>
       <div className="px-6 py-12 h-screen flex flex-col justify-start items-center md:w-[500px] mx-auto">
         <div className="avatar w-full mb-6">
-          <canvas height={1000} width={1000} className="w-full rounded relative" ref={canvasRef}></canvas>
+          <Canvas canvasRef={canvasRef} height={1000} width={1000} />
         </div>
         {overlayFilled ? (
           <div className="w-full flex flex-row space-x-4">
-            <div className="w-[100px]">
-              <input id="image" type="file" className="w-0 h-0" onChange={handleUploadImage} />
-              <label htmlFor="image" className="btn btn-primary w-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                  >
-                    <path d="M2 19V9a2 2 0 0 1 2-2h.5a2 2 0 0 0 1.6-.8l2.22-2.96A.6.6 0 0 1 8.8 3h6.4a.6.6 0 0 1 .48.24L17.9 6.2a2 2 0 0 0 1.6.8h.5a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2" />
-                    <path d="M12 17a4 4 0 1 0 0-8a4 4 0 0 0 0 8" />
-                  </g>
-                </svg>
-              </label>
-            </div>
-            <div className="w-full">
-              <button className="btn btn-primary btn-outline w-full" onClick={handleDownload}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m12 16l4-5h-3V4h-2v7H8z" /><path fill="currentColor" d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2z" /></svg>
-                Download
-              </button>
-            </div>
+            <CameraButton handleUploadImage={handleUploadImage} className="w-[100px]" />
+            <DownloadButton handleDownload={handleDownload} />
           </div>
         ) : (
-          <div className="w-full">
-            <input id="image" type="file" className="w-0 h-0" onChange={handleUploadImage} />
-            <label htmlFor="image" className="btn btn-primary w-full">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                >
-                  <path d="M2 19V9a2 2 0 0 1 2-2h.5a2 2 0 0 0 1.6-.8l2.22-2.96A.6.6 0 0 1 8.8 3h6.4a.6.6 0 0 1 .48.24L17.9 6.2a2 2 0 0 0 1.6.8h.5a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2" />
-                  <path d="M12 17a4 4 0 1 0 0-8a4 4 0 0 0 0 8" />
-                </g>
-              </svg>
-              Pilih Foto
-            </label>
-          </div>
+          <CameraButton handleUploadImage={handleUploadImage} className="w-full" label="Pilih Foto" />
         )}
       </div>
     </main>
---
+++ src/components/CameraButton.tsx
@@ -0,0 +1,39 @@
+import { ChangeEvent } from "react"
+
+type CameraButtonPropsType = {
+  handleUploadImage: (e: ChangeEvent<HTMLInputElement>) => void,
+  className?: string,
+  label?: string
+}
+
+const CameraButton = (props: CameraButtonPropsType) => {
+  const { handleUploadImage, className, label } = props
+
+  return (
+    <div className={className}>
+      <input id="image" type="file" className="w-0 h-0" onChange={handleUploadImage} />
+      <label htmlFor="image" className="btn btn-primary w-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+          >
+            <path d="M2 19V9a2 2 0 0 1 2-2h.5a2 2 0 0 0 1.6-.8l2.22-2.96A.6.6 0 0 1 8.8 3h6.4a.6.6 0 0 1 .48.24L17.9 6.2a2 2 0 0 0 1.6.8h.5a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2" />
+            <path d="M12 17a4 4 0 1 0 0-8a4 4 0 0 0 0 8" />
+          </g>
+        </svg>
+        { label }
+      </label>
+    </div>
+  )
+}
+
+export default CameraButton
