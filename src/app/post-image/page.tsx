e48f823278b4c5d71c63d174f82138934c7ce50b
juanangelaalma <juanangelaalma@gmail.com>
2024-04-01 00:53:59 +0700

    Fix useSearchParams() should be wrapped in a suspense boundary at page /post-image

--- src/app/post-image/page.tsx
+++ src/app/post-image/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, Suspense, useState } from "react";
 import * as htmlToImage from 'html-to-image';
 import { default as NextImage } from "next/image";
 import CameraButton from "@/components/CameraButton";
@@ -43,28 +43,30 @@ export default function PostImage() {
   }
 
   return (
-    <main className="bg-[#F3F4F6] min-h-screen">
-      <div className="px-6 py-12 h-screen flex flex-col justify-start items-center mx-auto">
-        <div className="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200">
-          <div
-            aria-hidden="true"
-            className={`inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-purple-500 to-white blur-2xl opacity-25`}
-          />
-          <div className="mb-6 w-[400px] h-[400px] relative rounded-lg overflow-hidden" id="resultComponent">
-            <NextImage style={{ opacity: isRepositioning ? 0.5 : 1 }} className="top-0 left-0 z-10 w-full md:max-w-[500px] max-auto absolute" src={`/assets/${twibbonFileName}`} width={500} height={500} alt="Twibbon" />
-            <DraggableImage style={{ opacity: isRepositioning ? 0.5 : 1, zIndex: isRepositioning ? 20 : 0 }} className="md:max-w-[500px] mx-auto" src={overlayImage} />
-          </div>
-          {overlayImage ? (
-            <div className="w-full flex flex-row space-x-4 md:max-w-[500px]">
-              <RepositioningButton isRepositioning={isRepositioning} handleRepositioning={handleRepositioning} />
-              <CameraButton handleUploadImage={handleUploadImage} />
-              <DownloadButton handleDownload={handleDownload} />
+    <Suspense>
+      <main className="bg-[#F3F4F6] min-h-screen">
+        <div className="px-6 py-12 h-screen flex flex-col justify-start items-center mx-auto">
+          <div className="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200">
+            <div
+              aria-hidden="true"
+              className={`inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-purple-500 to-white blur-2xl opacity-25`}
+            />
+            <div className="mb-6 w-[400px] h-[400px] relative rounded-lg overflow-hidden" id="resultComponent">
+              <NextImage style={{ opacity: isRepositioning ? 0.5 : 1 }} className="top-0 left-0 z-10 w-full md:max-w-[500px] max-auto absolute" src={`/assets/${twibbonFileName}`} width={500} height={500} alt="Twibbon" />
+              <DraggableImage style={{ opacity: isRepositioning ? 0.5 : 1, zIndex: isRepositioning ? 20 : 0 }} className="md:max-w-[500px] mx-auto" src={overlayImage} />
             </div>
-          ) : (
-            <CameraButton handleUploadImage={handleUploadImage} className="w-full md:max-w-[500px]" label="Pilih Foto" />
-          )}
+            {overlayImage ? (
+              <div className="w-full flex flex-row space-x-4 md:max-w-[500px]">
+                <RepositioningButton isRepositioning={isRepositioning} handleRepositioning={handleRepositioning} />
+                <CameraButton handleUploadImage={handleUploadImage} />
+                <DownloadButton handleDownload={handleDownload} />
+              </div>
+            ) : (
+              <CameraButton handleUploadImage={handleUploadImage} className="w-full md:max-w-[500px]" label="Pilih Foto" />
+            )}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </Suspense>
   );
 }
